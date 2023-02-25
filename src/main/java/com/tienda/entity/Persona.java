d768ab5d6717cea853fde56a678c10b5da3ff3b9
AnthonyDIP18 <anthonydip18@hotmail.com>
2023-02-24 18:41:55 -0600

    Semana 5

---
+++ src/main/java/com/tienda/entity/Persona.java
@@ -0,0 +1,124 @@
+package com.tienda.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table (name="paises")
+public class Persona implements Serializable{
+    @Id
+
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String setNombre(String nombre){
+        return nombre;
+    }
+    
+    private String nombre;
+    private String apellido1;
+    private String apellido2;
+    private String telefono;
+    private String email;
+    
+    private String password;
+    private int active;
+    private String roles = "";
+    private String permission = "";
+    
+    @ManyToOne
+    @JoinColumn(name = "paises_id")
+    private Pais pais;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+    
+    
+}
