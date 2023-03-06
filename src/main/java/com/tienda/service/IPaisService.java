dcf8ae3db990d913bc516d3787710a4cf712eeb3
AnthonyDIP18 <anthonydip18@hotmail.com>
2023-03-05 18:06:52 -0600

    Semana 7

---
+++ src/main/java/com/tienda/service/IPaisService.java
@@ -0,0 +1,15 @@
+package com.tienda.service;
+
+import com.tienda.entity.Pais;
+import java.util.List;
+
+/**
+ *
+ * @author Anthony
+ */
+
+
+
+public interface IPaisService {
+    public List<Pais> listCountry();
+}
